bac4b88a7586e88ba7a0b57599ab376768d92f5a
Damian Giaccobasso <damian_giaccobasso_tmg@whirlpool.com>
2024-05-10 14:30:10 -0300

    remove console.log(s)

--- src/app/(general)/questions/page.tsx
+++ src/app/(general)/questions/page.tsx
@@ -6,24 +6,25 @@ import { useGlobalContext } from "@/app/context/TriviaContext";
 import { getQuestions } from "@/app/fetchers/questions";
 
 const QuestionsPage = () => {
-  const { triviaParams } = useGlobalContext()
+  const { triviaParams } = useGlobalContext();
 
-  
-  const amount = triviaParams[0]
-  const category = triviaParams[1]
-  const difficulty = triviaParams[2]
+  const amount = triviaParams[0];
+  const category = triviaParams[1];
+  const difficulty = triviaParams[2];
 
   const { data: questions } = useQuery({
     queryKey: ["questions"],
     queryFn: () => getQuestions({ amount, category, difficulty }),
   });
 
-  console.log('fromquestionscomp', questions)
-
-  return <div className="text-white">
-    QuestionsPage
-    {questions?.map(question => <p key={question.question}>{question.question}</p>)}
-  </div>;
+  return (
+    <div className="text-white">
+      QuestionsPage
+      {questions?.map((question) => (
+        <p key={question.question}>{question.question}</p>
+      ))}
+    </div>
+  );
 };
 
 export default QuestionsPage;
--- src/app/fetchers/questions.ts
+++ src/app/fetchers/questions.ts
@@ -1,11 +1,10 @@
 import { QuestionsResponse } from "../interfaces/questions-response";
 
-export const getQuestions = async ({amount, category, difficulty}: { amount: number; category: number; difficulty: string }) => {
+export const getQuestions = async ({ amount, category, difficulty }: { amount: number; category: number; difficulty: string }) => {
   const apiUrl = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
   try {
     const resp = await fetch(apiUrl);
     const questions: QuestionsResponse = await resp.json();
-    console.log('from helper',questions.results)
     return questions.results
   } catch (err) {
     console.error(err);
